c53b2fb6385f8774be5c624872f27cdb1d1a57e7
Administrator <admin@example.com>
2021-05-15 20:04:27 +0530

    removed whitespace & added comments

--- index.js
+++ index.js
@@ -1,38 +1,31 @@
+//welcome to note app
+//function showing data on page load
 showNote()
 
+//getting html element
 let addBtn = document.getElementById('addBtn');
 let addTxt = document.getElementById('addTxt');
 let title = document.getElementById('title');
- 
-let d = new Date();
-let currentMonth = ['Jan','feb','Mar','Apr','May','June','July','Aug','Sept','Oct','Nov','Dec'];
-let day = d.getDate();
-let month = currentMonth[d.getMonth()];
-let year = d.getFullYear();
-let hour = d.getHours();
-let Min = d.getMinutes();
-let time = `${day}.${month}.${year}.${hour}:${Min}`  
-
-console.log(time)
+ //addEventListner to addbBtn to add note
 addBtn.addEventListener("click",getNotes);
-
-function getNotes(){
-   let notes= localStorage.getItem("note");
+//funtion to add note
+ function getNotes(){
+ 
+  let notes= localStorage.getItem("note");
    if(notes == null){
-       notesArray = [];
-   }
+       notesArray = []; 
+    }
    else{
-       notesArray = JSON.parse(notes);
-   }
-  
-   notesArray.unshift(addTxt.value);
-   console.log(notesArray)
+       notesArray = JSON.parse(notes); 
+    }
+
+    notesArray.unshift(addTxt.value);
+   console.log(notesArray);
    localStorage.setItem("note" , JSON.stringify(notesArray));
    addTxt.value = "";
-   
    showNote()
 }
-
+//show note on html page
 function showNote(){
     let notes= localStorage.getItem("note");
    if(notes == null){
@@ -41,46 +34,35 @@ function showNote(){
    else{
        notesArray = JSON.parse(notes);
    }
-   
    let html ='';
 notesArray.forEach((element , index )=> {
-     
-
-   html +=`
+    html +=`
    <div class="noteCard my-3 mx-3 card" style="width: 18rem;  ">
    <button  onclick="deleteNote(${index})" class="btn p-2 mt-2"  style="margin-left:90%; margin-top:0;margin-bottom:0;text-align:center">x</button>
-
-   <div class="card-body">
+  <div class="card-body">
    <h5 class="card-title">${index +1 }</h5>
     <p class="card-text"style="overflow:hidden"> ${element}</p>
    </div>
    <button id="${index}" onclick="editNote(this.id)" class="btn btn-dark my-2" style="width:100% ; margin-bottom:0;" >edit</button>
-
-</div>
+ </div>
    `});
-
-   let notesElm = document.getElementById("noteTxt");
+  let notesElm = document.getElementById("noteTxt");
    if (notesArray.length != 0) {
      notesElm.innerHTML = html;
    } else {
      notesElm.innerHTML = `Nothing to show! Use "Add a Note" section above to add notes.`;
    }
 }
-
+//function to edit note
 function editNote(index){
- 
-    
-    let notes= localStorage.getItem("note");
+   let notes= localStorage.getItem("note");
     if(notes == null){
         notesArray = [];
     }
     else{
         notesArray = JSON.parse(notes);
     }
- 
   addTxt.value= notesArray[index];
-   
-  
     document.getElementById('addBtn').style.display= "none";
     document.getElementById('edit-box').style.display= "block";
     document.getElementById('save').addEventListener("click" , function(){
@@ -94,27 +76,22 @@ else{var  task = addTxt.value;
         
         localStorage.setItem("note" , JSON.stringify(notesArray));
         addTxt.value="";
-    }
-         
-         showNote();
+    } 
+    showNote();
   closeEditBox()
- 
-   
- } 
- ) }
+ })}
+ //function to hide when ading note and show while editing.
    function closeEditBox(){
     document.getElementById('edit-box').style.display= "none";
     document.getElementById('addBtn').style.display= "block";
-
-   }
-   closeEditBox()   
-
+}
+ closeEditBox()   
+ //function to cancel editing.
 function cancel(){
      document.getElementById('edit-box').style.display= "none";
       document.getElementById('addTxt').value="";
-    
-}
- 
+ }
+ //function to delete note.
 function deleteNote(index){
     console.log('delete note')
     localStorage.removeItem(index)
@@ -125,8 +102,6 @@ function deleteNote(index){
    else{
        notesArray = JSON.parse(notes);
    }
-   
-   
  if(confirm("are you sure ?") == false){
      notesArray[index];
      localStorage.setItem("note", JSON.stringify(notesArray));
@@ -138,9 +113,9 @@ function deleteNote(index){
  }
  showNote()
 }
-
-
-let input = document.getElementById('input');
+//search note
+ let input = document.getElementById('input');
+ //addEventListner to search inputbox to find matching note  
  input.addEventListener("input" , function(){
      let inputVal = input.value ;
      console.log('input value' , inputVal);
@@ -152,6 +127,4 @@ let input = document.getElementById('input');
          } 
          else{
             ele.style.display = "none"
-         }
-     })
- });
+         }})});
